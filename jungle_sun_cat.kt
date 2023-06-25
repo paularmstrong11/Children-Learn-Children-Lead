d0646357e5d00293bac4d79983055c2c0609a525
paularmstrong11 <136752387+paularmstrong11@users.noreply.github.com>
2023-06-25 14:22:16 +0700

    Fix UI layout issues

---
+++ jungle_sun_cat.kt
@@ -0,0 +1,358 @@
+fun main() {
+   
+//1
+    var childName: String = "Jack"    
+//2
+    var childAge: Int = 10
+//3
+    var childClass: String = "4th Grade"
+//4
+    var childTeacher: String = "Mrs. Smith"
+//5
+    val childParent: String = "John & Jane"
+//6
+    val schoolName: String = "Willowbrooke Elementary School"
+//7
+    var childHobby: String = "Playing Video Games"
+//8
+    var childFriend: String = "James"
+//9
+    println("Name: $childName")
+//10
+    println("Age: $childAge")
+//11
+    println("Grade: $childClass")
+//12
+    println("Teacher: $childTeacher")
+//13
+    println("Parents: $childParent")
+//14
+    println("School: $schoolName")
+//15
+    println("Hobby: $childHobby")
+//16
+    println("Friend: $childFriend")
+//17
+    var childMathGrade: String = "A"
+//18
+    println("Math Grade: $childMathGrade") 
+//19
+    var childEnglishGrade: String = "A-"
+//20
+    println("English Grade: $childEnglishGrade")
+//21
+    val lessonsLearned: String = "To work hard and never give up"
+//22
+    println("Lessons Learned: $lessonsLearned")
+//23
+    var childFavoriteSubject: String = "Science"
+//24
+    println("Favorite Subject: $childFavoriteSubject")
+//25
+    val lessonsFromParents: String = "Be respectful to everyone"
+//26
+    println("Lessons From Parents: $lessonsFromParents")
+//27
+    val childBehavior: String = "Good"
+//28 
+    println("Behavior: $childBehavior")
+//29
+    val childsDream: String = "To become an astronaut"
+//30 
+    println("Dream: $childsDream")
+//31
+    var childSports: String = "Soccer"
+//32 
+    println("Sports: $childSports")
+//33
+    val childFavoriteBook: String = "Harry Potter and the Sorcerer's Stone"
+//34
+    println("Favorite Book: $childFavoriteBook")
+//35
+    var childMusic: String = "Rock Music"
+//36
+    println("Music: $childMusic")
+//37
+    val motto: String = "Work hard for what you want in life"
+//38
+    println("Motto: $motto")
+//39
+    val disciplineFromParents: String = "Know the difference between right and wrong"
+//40
+    println("Discipline From Parents: $disciplineFromParents")
+//41
+    val importanceOfReading: String = "To gain knowledge"
+//42
+    println("Importance of Reading: $importanceOfReading")
+//43
+    val importanceOfExercise: String = "It helps you stay healthy"
+//44
+    println("Importance of Exercise: $importanceOfExercise")
+//45
+    var childRecreation: String = "Swimming"
+//46
+    println("Recreation: $childRecreation")
+//47
+    val importanceOfGoodHygiene: String = "It keeps you healthy and looking good"
+//48
+    println("Importance of Good Hygiene: $importanceOfGoodHygiene")
+//49
+    val importanceOfTeamwork: String = "To achieve something together"
+//50
+    println("Importance of Teamwork: $importanceOfTeamwork")
+//51
+    val importanceOfListening: String = "To understand what others are saying"
+//52
+    println("Importance of Listening: $importanceOfListening")
+//53
+    val importanceOfFollowingRules: String = "To stay safe and be in control"
+//54
+    println("Importance of Following Rules: $importanceOfFollowingRules")
+//55
+    val importanceOfStudying: String = "To get good grades"
+//56
+    println("Importance of Studying: $importanceOfStudying")
+//57
+    val importanceOfGoodGrade: String = "It opens up doors to better opportunities"
+//58
+    println("Importance of Good Grade: $importanceOfGoodGrade")
+//59
+    val importanceOfLiteracy: String ="It helps you think critically"
+//60
+    println("Importance of Literacy: $importanceOfLiteracy")
+//61
+    var childSocialMedia: String = "Instagram"
+//62
+    println("Social Media: $childSocialMedia")
+//63
+    val importanceOfSocializing: String = "To make new friends"
+//64
+    println("Importance of Socializing: $importanceOfSocializing")
+//65
+    val importanceOfTimeManagement: String = "To be able to work efficiently"
+//66
+    println("Importance of Time Management: $importanceOfTimeManagement")
+//67
+    val importanceOfManners: String = "To act responsibly"
+//68
+    println("Importance of Manners: $importanceOfManners")
+//69
+    val importanceOfBeingPolite: String = "To show respect for others"
+//70
+    println("Importance of Being Polite: $importanceOfBeingPolite")
+//71
+    var childGame: String = "Mario Kart"
+//72
+    println("Game: $childGame")
+//73
+    val importanceOfImagination: String = "To think up new ideas"
+//74
+    println("Importance of Imagination: $importanceOfImagination")
+//75
+    val importanceOfCreativity: String = "To have something unique to share with the world"
+//76
+    println("Importance of Creativity: $importanceOfCreativity")
+//77
+    val importanceOfPunctuality: String = "To show up on time"
+//78
+    println("Importance of Punctuality: $importanceOfPunctuality")
+//79
+    val importanceOfDigitalCitizenship: String = "To behave ethically online"
+//80
+    println("Importance of Digital Citizenship: $importanceOfDigitalCitizenship")
+//81
+    val importanceOfConfidence: String = "It helps you be successful"
+//82
+    println("Importance of Confidence: $importanceOfConfidence")
+//83
+    val importanceOfRespect: String = "To show people that you value them" 
+//84
+    println("Importance of Respect: $importanceOfRespect")
+//85
+    var childInstrument: String = "Guitar"
+//86
+    println("Instrument: $childInstrument")
+//87
+    val importanceOfCooperation: String = "To work together effectively"
+//88
+    println("Importance of Cooperation: $importanceOfCooperation")
+//89
+    val importanceOfCriticalThinking: String = "To make logical decisions"
+//90
+    println("Importance of Critical Thinking: $importanceOfCriticalThinking")
+//91
+    val importanceOfPerseverance: String = "To keep trying and never give up"
+//92
+    println("Importance of Perseverance: $importanceOfPerseverance")
+//93
+    val importanceOfSelfControl: String = "To be able to resist temptations"
+//94
+    println("Importance of Self Control: $importanceOfSelfControl")
+//95
+    val importanceOfHonesty: String = "To maintain your integrity"
+//96
+    println("Importance of Honesty: $importanceOfHonesty")
+//97
+    val importanceOfEmpathy: String = "It helps you understand other people's feelings"
+//98
+    println("Importance of Empathy: $importanceOfEmpathy")
+//99
+    val importanceOfFriendship: String = "To make life more enjoyable"
+//100
+    println("Importance of Friendship: $importanceOfFriendship")
+//101
+    var childPets: String = "Goldfish"
+//102
+    println("Pets: $childPets")
+//103
+    val importanceOfTolerance: String = "To respect and accept the differences between people"
+//104
+    println("Importance of Tolerance: $importanceOfTolerance")
+//105
+    val importanceOfHealthyEating: String = "To stay healthy and strong"
+//106
+    println("Importance of Healthy Eating: $importanceOfHealthyEating")
+//107
+    val importanceOfSafety: String = "To prevent injury or harm"
+//108
+    println("Importance of Safety: $importanceOfSafety")
+//109
+    val importanceOfLeadership: String = "To lead and motivate others"
+//110
+    println("Importance of Leadership: $importanceOfLeadership")
+//111
+    val importanceOfHumor: String = "It helps put people at ease"
+//112
+    println("Importance of Humor: $importanceOfHumor")
+//113
+    val importanceOfPatience: String = "To stay calm in difficult situations"
+//114
+    println("Importance of Patience: $importanceOfPatience")
+//115
+    val importanceOfSelfReliance: String = "To learn to rely on yourself"
+//116
+    println("Importance of Self Reliance: $importanceOfSelfReliance")
+//117
+    val importanceOfTechnology: String = "To make life easier"
+//118
+    println("Importance of Technology: $importanceOfTechnology")
+//119
+    val importanceOfVolunteering: String = "To help others in need"
+//120
+    println("Importance of Volunteering: $importanceOfVolunteering")
+//121
+    val importanceOfResponsibility: String = "To be accountable for your actions"
+//122
+    println("Importance of Responsibility: $importanceOfResponsibility")
+//123
+    val importanceOfPrivacy: String = "To protect your personal information"
+//124
+    println("Importance of Privacy: $importanceOfPrivacy")
+//125
+    val importanceOfAdaptability: String = "To be able to adjust to different situations"
+//126
+    println("Importance of Adaptability: $importanceOfAdaptability")
+//127
+    val importanceOfKnowingLimits: String = "It helps you stay safe"
+//128
+    println("Importance of Knowing Limits: $importanceOfKnowingLimits")
+//129
+    val importanceOfResilience: String = "To bounce back from difficulties"
+//130
+    println("Importance of Resilience: $importanceOfResilience")
+//131
+    val importanceOfCompassion: String = "To understand and empathize with others"
+//132
+    println("Importance of Compassion: $importanceOfCompassion")
+//133
+    val importanceOfFlexibility: String = "To go with the flow and adjust to changing needs"
+//134
+    println("Importance of Flexibility: $importanceOfFlexibility")
+//135
+    val importanceOfSharing: String = "To show kindness towards others"
+//136
+    println("Importance of Sharing: $importanceOfSharing")
+//137
+    val importanceOfPositivity: String = "It helps keep up your spirits"
+//138
+    println("Importance of Positivity: $importanceOfPositivity")
+//139
+    val importanceOfConflictResolution: String = "To resolve disputes peacefully"
+//140
+    println("Importance of Conflict Resolution: $importanceOfConflictResolution")
+//141
+    val importanceOfFinancialLiteracy: String = "To be able to manage money and make wise decisions"
+//142
+    println("Importance of Financial Literacy: $importanceOfFinancialLiteracy")
+//143
+    val importanceOfCommunicationSkills: String = "To express yourself effectively"
+//144
+    println("Importance of Communication Skills: $importanceOfCommunicationSkills")
+//145
+    val importanceOfConcentration: String = "To stay focused on tasks"
+//146
+    println("Importance of Concentration: $importanceOfConcentration")
+//147
+    val importanceOfCaring: String = "To show kindness to people around you"
+//148
+    println("Importance of Caring: $importanceOfCaring")
+//149
+    val importanceOfCreativeThinking: String = "It helps you come up with unique solutions"
+//150
+    println("Importance of Creative Thinking: $importanceOfCreativeThinking")
+//151
+    val importanceOfSolvingProblems: String = "To find solutions to challenges"
+//152
+    println("Importance of Solving Problems: $importanceOfSolvingProblems")
+//153
+    val importanceOfResearchSkills: String = "To be able to conduct investigations"
+//154
+    println("Importance of Research Skills: $importanceOfResearchSkills")
+//155
+    val importanceOfOrganizationalSkills: String = "To be able to manage time and tasks efficiently"
+//156
+    println("Importance of Organizational Skills: $importanceOfOrganizationalSkills")
+//157
+    val importanceOfDecisionMaking: String = "To be able to choose the best solution"
+//158
+    println("Importance of Decision Making: $importanceOfDecisionMaking")
+//159
+    val importanceOfProblemSolving: String = "To find solutions to complex issues"
+//160
+    println("Importance of Problem Solving: $importanceOfProblemSolving")
+//161
+    val importanceOfAccuracy: String = "To ensure that work is done correctly"
+//162
+    println("Importance of Accuracy: $importanceOfAccuracy")
+//163
+    val importanceOfGrit: String = "To persist in the face of obstacles"
+//164
+    println("Importance of Grit: $importanceOfGrit")
+//165
+    val importanceOfThinkingPositively: String = "To stay motivated and focused"
+//166
+    println("Importance of Thinking Positively: $importanceOfThinkingPositively")
+//167
+    val importanceOfCopingSkills: String = "To be able to manage stressful situations"
+//168
+    println("Importance of Coping Skills: $importanceOfCopingSkills")
+//169
+    val importanceOfPresentationSkills: String = "To be able to effectively convey your thoughts and ideas"
+//170
+    println("Importance of Presentation Skills: $importanceOfPresentationSkills")
+//171
+    val importanceOfCollaboration: String = "To work together as a team to achieve common goals"
+//172
+    println("Importance of Collaboration: $importanceOfCollaboration")
+//173
+    val importanceOfPasswordManagement: String = "To keep your accounts safe"
+//174
+    println("Importance of Password Management: $importanceOfPasswordManagement")
+//175
+    val importanceOfMentalHealth: String = "To stay healthy both physically and mentally"
+//176
+    println("Importance of Mental Health: $importanceOfMentalHealth")
+//177
+    val importanceOfSelfDiscipline: String = "To stay on track and reach your goals"
+//178
+    println("Importance of Self Discipline: $importanceOfSelfDiscipline")

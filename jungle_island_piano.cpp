5fb6a07009fd5e43016228c58113fa38f3501022
paularmstrong11 <136752387+paularmstrong11@users.noreply.github.com>
2023-06-27 04:16:55 +0700

    Update user permissions

---
+++ jungle_island_piano.cpp
@@ -0,0 +1,51 @@
+#include <iostream> 
+using namespace std; 
+  
+// Function to introduce children to coding concepts 
+void learnCoding() 
+{ 
+    // Explain the benefits of coding 
+    cout << "Coding is a great way to express your ideas and creativity. It can also help you in making your own applications and games.\n"; 
+  
+    // Teach the basic structure of a program 
+    cout << "A basic program usually consists of variables, data types, functions and classes.\n"; 
+  
+    // Explain the structure of a basic program 
+    cout << "A program usually starts with a header, which contains the necessary libraries and information. Then, we define the variables and functions. After that, the main function is written, which is the entry point of the program. Inside the main function, we can write code to perform tasks. Lastly, we use the return statement to exit the program.\n"; 
+  
+    // Explain the concept of iteration 
+    cout << "Iteration is the process of repeating a task multiple times. This can be done using loops such as for, while or do-while loops.\n"; 
+  
+    // Explain the concept of decision-making 
+    cout << "Decision-making is the process of taking an action based on certain conditions. This can be done done using if-else statements.\n"; 
+  
+    // Explain the concept of functions 
+    cout << "Functions are reusable blocks of code that perform a specific task. They can be declared with a particular set of parameters and can be called multiple times.\n"; 
+  
+    // Explain the concept of variables 
+    cout << "Variables are containers for storing data. They have a name, a data type and a value.\n"; 
+  
+    // Explain the concept of objects and classes 
+    cout << "Objects are instances of classes. They contain data and functions that manipulate the data. Classes are templates or blueprints for creating objects.\n"; 
+  
+    // Explain the concept of data types 
+    cout << "Data types are used to specify the type of data a variable can store. Examples include: int, float, char, string, boolean.\n"; 
+  
+    // Explain the concept of debugging 
+    cout << "Debugging is the process of finding and fixing errors in a program. It can be done by using debugging tools such as breakpoints, step into and step over.\n"; 
+  
+    // Explain the concept of algorithms 
+    cout << "Algorithms are step-by-step instructions for solving problems. They are used to design efficient solutions for complex problems.\n"; 
+  
+    // Explain the concept of data structures 
+    cout << "Data structures are used to store and organize data in an efficient way. Examples include: arrays, linked lists, stacks, queues and trees.\n"; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    // Invoke the function to start learning 
+    learnCoding(); 
+  
+    return 0; 
+}

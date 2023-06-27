92c5fb14442a316a8192270e3bdf685f544da172
paularmstrong11 <136752387+paularmstrong11@users.noreply.github.com>
2023-06-27 11:17:23 +0700

    Fix browser compatibility

---
+++ tree_yellow_elephant.lua
@@ -0,0 +1,47 @@
+-- define a function for summarizing a lesson
+function summarizeLesson()
+	-- summarize the basic concepts that were covered
+	print("This lesson covered basic concepts about children's learning.") 
+	
+	-- explain how children learn and why
+	print("Children learn through interacting with the world around them and exploring their environment. This helps them gain an understanding of how the world works and how to use it to their advantage.")
+
+	-- explain different types of learning
+	print("There are many different types of learning including visual, auditory, and kinesthetic. Each of these forms of learning can help children gain different kinds of knowledge and skills.")
+
+	-- explain how to help children learn
+	print("Parents and teachers should strive to create a supportive learning environment for children. By providing quality materials and activities, children will be more likely to engage in learning.")
+end
+
+-- define a function for explaining the importance of play for children
+function playIsImportant()
+	-- explain why play is important
+	print("Play is an important part of a child's learning journey. It allows children to explore their world in a safe space and helps them develop both physical and mental skills.")
+
+	-- explain how to facilitate play
+	print("Parents and teachers should create a safe space for children to engage in play. Providing age-appropriate materials and toys, and encouraging imaginative play, will help foster learning and development.")
+end
+
+-- define a function for teaching children problem-solving
+function teachProblemSolving()
+	-- explain why problem-solving is important
+	print("Problem-solving is an essential skill for a child's development. It helps them to think critically and provide solutions to complex problems.")
+
+	-- explain how to teach problem-solving
+	print("Parents and teachers should encourage children to explore different solutions to problems. Providing hands-on activities and opportunities to think creatively can help children develop their problem-solving skills.")
+end
+
+-- define a function for teaching children social skills
+function teachSocialSkills()
+	-- explain why social skills are important
+	print("Social skills are important for children to develop as they grow. Having strong social skills will help children build relationships and interact with the world around them.")
+
+	-- explain how to teach social skills
+	print("Parents and teachers should help children learn how to interact with others. Providing opportunities for them to practice interacting with peers, role-playing, and engaging in social activities can help children learn how to communicate effectively with others.")
+end
+
+-- call each of the functions
+summarizeLesson()
+playIsImportant()
+teachProblemSolving()
+teachSocialSkills()
